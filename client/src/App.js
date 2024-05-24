33c443078ddcb48eb94e78614153423edeca80bc
KClower <kclower3@gmail.com>
2024-05-23 20:07:08 -0500

    finished project

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,40 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import UpdateMovieForm from "./Movies/UpdateMovieForm";
 import axios from 'axios';
 
 const App = () => {
+
   const [savedList, setSavedList] = useState([]);
   const [movieList, setMovieList] = useState([]);
+
+  const updateMovieList = (movie) => {
+    const newMovies = movieList.map((m) => {
+      if (m.id === movie.id) {
+        return movie
+      } else {
+        return m
+      }
+    })
+    setMovieList(newMovies)
+  }
+
+  const deleteMovie = (movieId) => {
+    const newMoviesList = movieList.filter((m) => {
+      return m.id !== movieId
+    })
+    setMovieList(newMoviesList)
+    const foundIndex = savedList.map(movie => movie.id).indexOf(movieId)
+    if (foundIndex > -1) {
+      const newSavedList = savedList.filter((movie) => {
+        return movie.id !== movieId
+      })
+      setSavedList(newSavedList)
+    }
+  }
+
+
 
   const getMovieList = () => {
     axios
@@ -28,12 +57,16 @@ const App = () => {
     <>
       <SavedList list={savedList} />
 
-      <Route exact path="/">
-        <MovieList movies={movieList} />
+      <Route exact path="/" render={(routeProps) => <MovieList {...routeProps} movies={movieList} />} />
+
+
+
+      <Route exact path="/movies/:id">
+        <Movie addToSavedList={addToSavedList} deleteMovie={deleteMovie} />
       </Route>
 
-      <Route path="/movies/:id">
-        <Movie addToSavedList={addToSavedList} />
+      <Route path="/movies/movie-form/:id">
+        <UpdateMovieForm UpdateMovieForm={UpdateMovieForm} updateMovieList={updateMovieList} />
       </Route>
     </>
   );
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { useHistory } from 'react-router-dom';
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
-function Movie({ addToSavedList }) {
+function Movie({ addToSavedList, deleteMovie }) {
+  const history = useHistory()
   const [movie, setMovie] = useState(null);
   const params = useParams();
+
+
+
 
   const fetchMovie = (id) => {
     axios
@@ -26,12 +31,33 @@ function Movie({ addToSavedList }) {
     return <div>Loading movie information...</div>;
   }
 
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    axios
+      .delete(`http://localhost:5000/api/movies/${movie.id}`)
+      .then((res) => {
+        deleteMovie(res.data)
+        history.push("/")
+      })
+      .catch((err) => console.log(err))
+  }
+
+
   return (
     <div className="save-wrapper">
       <MovieCard movie={movie} />
 
       <div className="save-button" onClick={saveMovie}>
         Save
+      </div>
+
+      <div className="update-button" onClick={() => history.push(`/movies/movie-form/${movie.id}`)}>
+        Update
+      </div>
+
+      <div className="delete-button" onClick={handleDelete}>
+        Delete Movie
       </div>
     </div>
   );
